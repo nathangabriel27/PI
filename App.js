004394502397cca06cb5bda139f1f74a880c50c6
System Administrator <root@MacBook-Pro-de-Estevao.local>
2019-09-26 19:09:40 -0300

    configurado meu firebase

--- App.js
+++ App.js
@@ -28,12 +28,14 @@ export default class App extends Component<Props> {
       deviceHeight: height
     };
   }
-   
+
 
   componentWillMount() {
     //Posso Fazer qualquer tipo de configuração global aqui como por exemplo o Firebase
     if (firebase.apps.length === 0) {
       firebase.initializeApp({
+        /*
+        Firebase do cabaço
         apiKey: "AIzaSyDgaCFXCapkNEC4daieScKGIRCTbouWxuM",
         authDomain: "doc-pi.firebaseapp.com",
         databaseURL: "https://doc-pi.firebaseio.com",
@@ -42,7 +44,16 @@ export default class App extends Component<Props> {
         messagingSenderId: "784412510062",
         appId: "1:784412510062:web:234500d8917c637c7fe37f",
         measurementId: "G-T5BK0MYR4G"
+        */
 
+        apiKey: "AIzaSyB8H6wnED0kHxo5dDOeFqwIh1I6bc30BVk",
+        authDomain: "projetopi-1567619287933.firebaseapp.com",
+        databaseURL: "https://projetopi-1567619287933.firebaseio.com",
+        projectId: "projetopi-1567619287933",
+        storageBucket: "",
+        messagingSenderId: "478223848222",
+        appId: "1:478223848222:web:1af3866a6784781bee2f0f",
+        measurementId: "G-2WW4D4N5T7"
       })
     }
   }
