ec48f273ac86b057a9672f5fbd623d35a59fbe8e
Estevao Santos <estevao_santos7@hotmail.com>
2019-10-01 14:05:05 -0300

    Mapa OK, sem calcular a rota

--- src/screens/components/Directions/index.js
+++ src/screens/components/Directions/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import MapViewDirections from 'react-native-maps-directions';
 
-const Directions = ({ destinations, origin, onReady }) => (
-    <MapViewDirections 
-    destination={destination}
-    origin={origin}
-    onReady={onReady}
-    apikey="AIzaSyAtuG77eNLPhsO28nm5Ua_DDSClMxYypdA"
-    strokeWidth={3}
-    strokeColor="#222"
+const Directions = ({ destination, origin, onReady }) => (
+    <MapViewDirections
+        destination={destination}
+        origin={origin}
+        onReady={onReady}
+        apiKey='AIzaSyAtuG77eNLPhsO28nm5Ua_DDSClMxYypdA'
+        strokeWidth={3}
+        strokeColor="#222"
     />
 );
 
